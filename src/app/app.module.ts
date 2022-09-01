ed54e66fe0068921e7770b092133a6601ee426a1
joselechuga <jos.medinav@duocuc.cl>
2022-09-01 16:30:10 -0400

    retoque login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,11 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import {HttpClientModule} from '@angular/common/http'
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule,BrowserAnimationsModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { createAnimation, NavController } from '@ionic/angular';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
-  
+  constructor(public navCtrl:NavController) { }
+    
+
   mostrar(){
     var usuario = JSON.parse(localStorage.getItem('usuario'));
 
@@ -21,7 +22,7 @@ export class PerfilPage implements OnInit {
     console.log(datosUsuario[1]);
     
     
-    /**
+    /** opcion de obtencion de datos
     var a = {
       b : datosUsuario[1]
     }
@@ -31,6 +32,24 @@ export class PerfilPage implements OnInit {
 
 
   ngOnInit() {
+    const animation = createAnimation()
+   .addElement(document.querySelector('.square'))
+   .duration(1500)
+   .iterations(Infinity)
+   .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+   .fromTo('opacity', '1', '0.2');
+
+document.querySelector('#play').addEventListener('click', () => {
+  animation.play();
+});
+
+document.querySelector('#pause').addEventListener('click', () => {
+  animation.pause();
+});
+
+document.querySelector('#stop').addEventListener('click', () => {
+  animation.stop();
+});
   }
 
 }
