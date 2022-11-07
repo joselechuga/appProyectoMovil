791e9e600955b35508f81efcbb1115cf970fed6e
joselechuga <jos.medinav@duocuc.cl>
2022-11-07 12:40:00 -0300

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,18 +15,9 @@ export class HomePage {
   constructor(private activeroute: ActivatedRoute,
      private router: Router,
      public navCtrl : NavController) {
-   
-    this.activeroute.queryParams.subscribe(params => { 
-      if (this.router.getCurrentNavigation().extras.state) { 
-        this.data = this.router.getCurrentNavigation().extras.state.user;
-        console.log(this.data) 
-      }else{this.router.navigate(["/login"])} 
-    });
+
   }
 
+  
 
-
-  irtienda(){
-    this.router.navigate(['/producto'])
-  }
 }
